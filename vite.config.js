ff4b9ffd638a19eca3a8131782007e90378b72e2
callmearya <60613987+callmearya@users.noreply.github.com>
2024-10-25 23:12:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      // Specify any external modules to avoid Vite bundling them inappropriately
-      external: ['firebase/app', 'firebase/database'], 
-    },
-  },
+      external: ['main.js']
+    }
+  }
 });
